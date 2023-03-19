3b47f3188bf9f54f442f8bed0c934bb9a93e2bfd
angelo <angesmor12@hotmail.com>
2023-03-19 13:04:15 -0300

    angular3

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -67,9 +67,9 @@ export class DetailsComponent {
       next: (res:any) => {
         this.review = res;this.review.type = "png"; this.service.getUserReviews(this.review.user, this.routeParam).pipe(takeUntil(this.onDestroy$)).subscribe({
         next: (res:any) => {this.List = res; if(this.List == null){this.destroy = 1}},
-        error: (e:any) => console.log(e)
+        error: (e:any) => {if (e){this.router.navigate(["../home"])}
       })},
-      error: (e:any) => console.log(e)
+      error: (e:any) => {if (e){this.router.navigate(["../home"])}}
     })
   }
 
